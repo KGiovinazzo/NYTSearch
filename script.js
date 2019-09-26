0eeb2cad855f6570e51f363e058499255c022425
Joseph Young <joseph.young32@yahoo.com>
2019-09-25 21:24:12 -0400

    added our nyt api key to js file

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ $(".sub").click(function () {
     alert("This works");
     console.log(click);
 });
+
+$(this).on("click", function(){
+    var search = $("this").value("submit");
+});
+
+function display(){
+    var search= "";
+    var yourKey = "s4InJ9kSonkqA0pGDwn89mk6PzgTowPz";
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=" + yourKey;
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+        }).then(function(response){
+        
+            console.log(response);
+        })
+};
+
+});
